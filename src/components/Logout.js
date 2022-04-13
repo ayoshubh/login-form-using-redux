17dcb97c5156af078a39f0898082edac3d41a53b
Shubham Chauhan <cshubham1911@gmail.com>
2022-04-13 18:34:27 +0530

    first commit

---
+++ src/components/Logout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div>
+        <h1>Welcome <span>Shubham</span>
+            <button>Logout</button>
+        </h1>
+    </div>
+  )
+}
+
+export default Logout
